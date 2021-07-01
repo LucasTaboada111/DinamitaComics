8a57c03e2e8cdf7043d1955068df525e5925260f
LucasTaboada111 <lucasdanieltaboada@gmail.com>
2021-07-01 14:12:14 -0300

    commit para pullear

--- back/routes/orderDetail.js
+++ back/routes/orderDetail.js
@@ -1,21 +1,27 @@
 const router = require("express").Router();
-const { User,Comic,Order,OrderDetail } = require("../models");
+const { User, Comic, Order, OrderDetail } = require("../models");
 
 router.post("/addComic", (req, res, next) => {
-    const LoggedUser = req.user
-    const Comic = req.Comic
-    
-    Order.findAll({where:{userId: LoggedUser.id}})
-    .then((orders)=>{
-        OrderDetail.findOrCreate({where:{ id:orders.length + 1 }})
-        .then((order)=>{
-            User.findByPk(LoggedUser.id)
-            .then((usuario)=>{
-                usuario.addOrder_detail(order[0]);
-                res.send(order);
-            })
-        })
+  const LoggedUser = req.user;
+  const addedComic = req.body.comic;
+
+  Order.findAll({ where: { userId: LoggedUser.id } })
+    .then((orders) => {
+      OrderDetail.findOrCreate({ where: { id: orders.length + 1 } }).then(
+        (order) => {
+          User.findByPk(LoggedUser.id).then((usuario) => {
+              usuario.addOrder_detail(order[0]);
+            Comic.findByPk(addedComic.id).then((comic) => {
+              comic.addOrder_detail(order[0]);
+              res.status(200).send(order[0])
+            });
+          });
+        }
+      );
     })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
