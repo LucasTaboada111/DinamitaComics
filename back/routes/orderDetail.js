cfce1085cc2909312612e0d17f86e61c6901cfbd
LucasTaboada111 <lucasdanieltaboada@gmail.com>
2021-07-01 20:27:34 -0300

    refactoreando la ruta post del carrito

--- back/routes/orderDetail.js
+++ back/routes/orderDetail.js
@@ -6,9 +6,17 @@ router.post("/addComic", (req, res, next) => {
   const addedComic = req.body.comic;
   const cantidad = req.body.cantidad;
 
+
+
+
+
+  //La onda de esto es crear un carrito con el el id de l usuario y cuando se digne a comprar
+  // guardamos ese carrrito en algun lugar (para historial, probablemente tabla nueva) y
+  //le borramos al carajo la data del carrito de ese usuario, entonces le queda limpia para que pueda seguir 
+  //agregando y nos quedamos tranka de que la data anterior quedo guardad para mostrar.
   Order.findAll({ where: { userId: LoggedUser.id } })
     .then((orders) => {
-      OrderDetail.findOrCreate({ where: { id: orders.length + 1 } }).then(
+      OrderDetail.findOrCreate({ where: { id: LoggedUser.id } }).then(
         (order) => {
           Comic.findByPk(addedComic.id).then((comic) => {
             /* const id = comic.id */
@@ -30,5 +38,45 @@ router.post("/addComic", (req, res, next) => {
       next(err);
     });
 });
+
+
+
+
+
+
+/* router.post("/addComic", (req, res, next) => {
+  const LoggedUser = req.user;
+  const addedComic = req.body.comic;
+  const cantidad = req.body.cantidad;
+
+  Order.findAll({ where: { userId: LoggedUser.id } })
+    .then((orders) => {
+      OrderDetail.findOrCreate({ where: { id: orders.length + 1 } }).then(
+        (order) => {
+          Comic.findByPk(addedComic.id).then((comic) => {
+            
+            order[0]
+              .update({
+                products: [...order[0].products, { comic, cantidad }],
+              })
+              .then((orderActualizada) => {
+                User.findByPk(LoggedUser.id).then((usuario) => {
+                  usuario.addOrder_detail(order[0]);
+                  res.status(200).send(orderActualizada);
+                });
+              });
+          });
+        }
+      );
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+
+ */
+
+
 
 module.exports = router;
