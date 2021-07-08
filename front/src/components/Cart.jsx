8c4ec7e4fb0e8997dce7818ccff39668ed7a9b57
Gustavo <gusegonzalez97@gmail.com>
2021-07-08 11:36:07 -0300

    arreglo boton y date

--- front/src/components/Cart.jsx
+++ front/src/components/Cart.jsx
@@ -100,14 +100,16 @@ const DataTableTemplatingDemo = () => {
         style={{ width: "20%", margin: "0 auto" }}>
         Buy Cart
       </Button>
-       <Link to = "/history">
+      
       <Button
         icon="pi pi-wallet"
         className={styles.buttonBuy}
         style={{ width: "20%", margin: "0 auto" }}>
+ <Link to = "/history">
         Shopping history
+         </Link>
       </Button>
-      </Link>
+     
     </div>
   )
 
